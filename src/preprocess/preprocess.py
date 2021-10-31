06658b88cd8802bd32d6dc2bfde3ec200eda5e26
huangmingg <huangmingg@hotmail.com>
2021-10-31 19:47:34 +0800

    Update preprocess.py

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from sampling.sampling import SamplingAlgorithm
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from typing import Tuple
 import os
@@ -19,7 +20,7 @@ class DatasetWrapper(ABC):
         pass
 
     def balance(self, algorithm: SamplingAlgorithm) -> None:
-        self.bxt, self.yxt = algorithm.run(self.x_train, self.y_train)
+        self.bxt, self.yxt = algorithm.run(self.x_train, self.y_train, columns=self.columns)
 
 
 class CreditCardDataset(DatasetWrapper):
@@ -29,16 +30,17 @@ class CreditCardDataset(DatasetWrapper):
 
     def preprocess(self) -> None:
         """Initializes attribute x_train, x_test, y_train, y_test"""
+
+        self.raw_df = self.raw_df[~self.raw_df.duplicated(keep='last')]
+
         y = self.raw_df['Class']
 
         # drop unnecessary columns
         x = self.raw_df.drop(['Class', 'Time'], axis=1)
         self.columns = x.columns
 
-        # handle missing data
-
-        # scale data
-
         # split data
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x.to_numpy(), y.to_numpy(), train_size=0.80, random_state=4012)
-
+        scaler = preprocessing.StandardScaler()
+        self.x_train = scaler.fit_transform(self.x_train)
+        self.x_test = scaler.transform(self.x_test)
