64b35efd4bc49aa7f4564823081de8bfc203e209
Rance <limjyrance@gmail.com>
2021-10-31 17:19:10 +0800

    Remove duplicates

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -33,6 +33,8 @@ class CreditCardDataset(DatasetWrapper):
 
     def preprocess(self) -> None:
         """Initializes attribute x_train, x_test, y_train, y_test"""
+
+        self.raw_df = self.raw_df.drop_duplicates()
         y = self.raw_df['Class']
 
         # drop unnecessary columns
@@ -58,23 +60,27 @@ class AdultDataset(DatasetWrapper):
         # handle missing data        
         self.raw_df[self.raw_df=='?'] = np.nan
         self.raw_df = self.raw_df.dropna(subset=['workclass', 'occupation', 'native.country'])
-        
-        # drop unnecessary columns
+
+        # drop duplicates
+        self.raw_df = self.raw_df.drop_duplicates()
+
+        # identify numerical and categorical columns
         x = self.raw_df.drop('income', axis=1)
         y = self.raw_df.income
         self.columns = x.columns
-        
+        self.num_columns = ['age', 'fnlwgt', 'education.num', 'capital.gain', 'capital.loss', 'hours.per.week']
+        self.cat_columns = ['workclass', 'education', 'marital.status', 'occupation', 'relationship' ,'race', 'sex', 'native.country']  
+        self.num_columns_ind = [self.columns.tolist().index(num) for num in self.num_columns]
+        self.cat_columns_ind = [self.columns.tolist().index(cat) for cat in self.cat_columns]      
         
         # split train-test
-        x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.80, random_state=4012)
+        x_train, x_test, y_train, y_test = train_test_split(x.to_numpy(), y.to_numpy(), train_size=0.80, random_state=4012)
         
         # scale numerical columns
-        num_cols = ['age', 'fnlwgt', 'education.num', 'capital.gain', 'capital.loss', 'hours.per.week']
         ss = StandardScaler()
-        x_train[num_cols] = ss.fit_transform(x_train[num_cols])
-        x_test[num_cols] = ss.transform(x_test[num_cols])
+        x_train[:,self.num_columns_ind] = ss.fit_transform(x_train[:,self.num_columns_ind])
+        x_test[:,self.num_columns_ind] = ss.transform(x_test[:,self.num_columns_ind])
         
-        #
         self.x_train = x_train
         self.x_test = x_test
         self.y_train = y_train
