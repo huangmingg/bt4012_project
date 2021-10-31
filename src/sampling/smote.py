593998a786c11ce59f3f060014fad42210cea1c5
Rance <limjyrance@gmail.com>
2021-10-31 17:19:53 +0800

    Add SMOTENC

--- src/sampling/smote.py
+++ src/sampling/smote.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from preprocess.preprocess import DatasetWrapper
 from sampling.sampling import SamplingAlgorithm
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, SMOTENC
 from imblearn.under_sampling import RandomUnderSampler 
 from imblearn.pipeline import Pipeline
 from typing import Tuple
@@ -11,11 +11,31 @@ class SmoteAlgorithm(SamplingAlgorithm):
     
     @staticmethod
     def run(x_train: np.array, y_train: np.array) -> Tuple[np.array, np.array]:
-        minority_ratio = len(y_train[y_train==0])*5
-        majority_ratio = len(y_train[y_train==1])*6
-        over = SMOTE(sampling_strategy={1: minority_ratio})
-        under = RandomUnderSampler(sampling_strategy={1: majority_ratio})
-        steps = [('o', over), ('u', under)]
-        pipeline = Pipeline(steps=steps)
-        balanced_x, balanced_y = pipeline.fit_resample(x_train, y_train)
+        # minority_ratio = len(y_train[y_train==0])*5
+        # majority_ratio = len(y_train[y_train==1])*6
+        # over = SMOTE(sampling_strategy={1: minority_ratio})
+        # under = RandomUnderSampler(sampling_strategy={1: majority_ratio})
+        # steps = [('o', over), ('u', under)]
+        # pipeline = Pipeline(steps=steps)
+        balanced_x, balanced_y = SMOTE().fit_resample(x_train, y_train)
         return balanced_x, balanced_y
+
+
+class SmotencAlgorithm(SamplingAlgorithm):
+    
+    @staticmethod
+    def run(x_train: np.array, y_train: np.array, **kwargs) -> Tuple[np.array, np.array]:
+        """Runs SMOTENC algorithm to balance dataset. Intended for mixed datasets (categorical + numerical features)
+
+        Args:
+            x_train (np.array): Array containing sample features, where shape is (n_samples, n_features)
+            y_train (np.array): Target vector relative to x_train
+            **categorical_features (list): List of indices of categorical features
+        Returns:
+            Tuple[np.array, np.array]: Tuple containing sample features and target vector of balanced dataset
+        """
+
+        categorical_features = kwargs['categorical_features']
+        balanced_x, balanced_y = SMOTENC(categorical_features=categorical_features).fit_resample(x_train, y_train)
+        return balanced_x, balanced_y
+
