31bca395a0ee7bf310463845d2a851327dab8f3d
huangmingg <huangmingg@hotmail.com>
2021-11-01 14:49:50 +0800

    Update mcd_smote.py

--- src/sampling/mcd_smote.py
+++ src/sampling/mcd_smote.py
@@ -9,34 +9,33 @@ from sklearn.covariance import MinCovDet
 
 
 class McdSmote(SamplingAlgorithm):
+    """Oversample using our customized experimental algorithm McdSmote.
+
+    Similar to robROSE, McdSMOTE uses MCD algorithm (mahalanobis distance) detect outliers of the minority class,
+    which is then removed from the resampling dataset. It then performs a SMOTE algorithm to generate the synthethic dataset
+    that only consists of the inlier minority class examples.
+
+    McdSmote currently only supports binary classification.
+    """
 
     @staticmethod
     def run(x_train: np.array, y_train: np.array, p: float=0.999, sp: float=0.95, random_state: int=4012, **kwargs) -> Tuple[np.array, np.array]:
-        fraud_index = np.where(y_train == 1)
-        normal_index = np.where(y_train == 0)
+        minority_index = np.where(y_train == 1)
+        majority_index = np.where(y_train == 0)
+        minority = x_train[minority_index]
+        majority = x_train[majority_index]
 
-        fraud = x_train[fraud_index]
-        normal = x_train[normal_index]
-        print(f'Number of original fraud rows = {fraud.shape[0]}') 
-        print(f'Number of original normal rows = {normal.shape[0]}') 
-
-        cov = MinCovDet(assume_centered=False, support_fraction=sp, random_state=random_state).fit(fraud)
-        md = cov.mahalanobis(fraud)
-        threshold = chi2.ppf(p, fraud.shape[1])
+        # runs MCD algorithm and generate the mahalanobis for the dataset
+        cov = MinCovDet(assume_centered=False, support_fraction=sp, random_state=random_state).fit(minority)
+        md = cov.mahalanobis(minority)
+        threshold = chi2.ppf(p, minority.shape[1])
 
         outliers_index = np.where(md >= threshold)
         inliers_index = np.where(md < threshold)
-        print(f'Number of outlier fraud rows = {len(outliers_index[0])}')
-        print(f'Number of inlier fraud rows = {len(inliers_index[0])}')
+        minority = minority[inliers_index]
+        new_x_train = np.concatenate((majority, minority), axis=0)
+        new_y_train = np.concatenate((np.zeros(majority.shape[0]), np.ones(minority.shape[0])), axis=None)
 
-        fraud = fraud[inliers_index]
-        print(f'Number of fraud rows remaining = {fraud.shape[0]}')
-        new_x_train = np.concatenate((normal, fraud), axis=0)
-        new_y_train = np.concatenate((np.zeros(normal.shape[0]), np.ones(fraud.shape[0])), axis=None)
-
-        print(f'Shape after removal of fraud outliers - {new_x_train.shape}')
-
+        # runs SMOTE algorithm to generate synthetic data for the inlier minority class examples
         balanced_x, balanced_y = SMOTE().fit_resample(new_x_train, new_y_train)
-        print(f'Number of balanced fraud rows = {len(np.where(balanced_y == 1)[0])}')
-        print(f'Number of balanced normal rows = {len(np.where(balanced_y == 0)[0])}')
         return balanced_x, balanced_y
