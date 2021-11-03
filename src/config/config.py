46f7d0154368b70c1ab6b85e20e0c0623745302e
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-03 19:43:18 +0800

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -4,7 +4,7 @@ from model.lg_model import LGWrapper
 from preprocess.preprocess import CreditCardDataset, SwarmDataset, AdultDataset
 from sampling.baseline import BaselineAlgorithm
 from sampling.robrose import RobRoseAlgorithm
-from sampling.adasyn import Adasyn
+from sampling.adasyn import AdasynAlgorithm
 from sampling.smote import SmoteAlgorithm
 from sampling.mcd_adasyn import McdAdasynAlgorithm
 from sampling.mcd_smote import McdSmoteAlgorithm
@@ -12,14 +12,14 @@ from sampling.sampling import SamplingAlgorithm
 
 
 DATASETS = [
-    (CreditCardDataset, 'creditcard_truncate.csv'),
+    (CreditCardDataset, 'creditcard.csv'),
     (SwarmDataset, 'Swarm_Behaviour.csv'),
     (AdultDataset, 'adult.csv')
     ]
 
 ALGORITHMS: SamplingAlgorithm = [
     RobRoseAlgorithm,
-    Adasyn,
+    AdasynAlgorithm,
     SmoteAlgorithm,
     McdAdasynAlgorithm,
     McdSmoteAlgorithm
