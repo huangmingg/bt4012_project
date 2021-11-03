520c740d295feb0b558e855316ebfd56596b5e38
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-03 19:50:56 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,17 @@
-from sampling.sampling import SamplingAlgorithm
-from model.model import ClassifierWrapper
-from preprocess.preprocess import DatasetWrapper
 from config.config import ALGORITHMS, DATASETS, MODELS
 
 
-def evaluate(d: DatasetWrapper, a: SamplingAlgorithm, m: ClassifierWrapper) -> None:
-    d.balance(a)
-    model = m(d)
-    model.train()
-    
 def main():
     datasets = [wrapper(filename) for wrapper, filename in DATASETS]
     for d in datasets:
         for a in ALGORITHMS:
             for m in MODELS:
-                evaluate(d, a, m)
+                d.balance(a)
+                model = m(d)
+                print(f"Evaluating {type(model).__name__} model for algorithm {a.__name__} using dataset {type(d).__name__}")
+                model.train()
+                model.evaluate()
+
 
 if __name__ == '__main__':
     main()
