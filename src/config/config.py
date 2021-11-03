c3ca56ab87ad1dccd70f15f81c80412e526f346f
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-03 12:04:54 +0800

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -2,20 +2,21 @@ from preprocess.preprocess import CreditCardDataset, AdultDataset
 from sampling.baseline import BaselineAlgorithm
 from sampling.robrose import RobRoseAlgorithm
 from sampling.smote import SmoteAlgorithm
-from sampling.mcd_adasyn import McdAdasyn
-from sampling.mcd_smote import McdSmote
+from sampling.mcd_adasyn import McdAdasynAlgorithm
+from sampling.mcd_smote import McdSmoteAlgorithm
 from sampling.sampling import SamplingAlgorithm
 from model.xgb_model import XGBWrapper
 from model.lg_model import LGWrapper
 
 
 DATASETS = [
-    (CreditCardDataset, 'creditcard_truncate.csv'),
+    (CreditCardDataset, 'creditcard.csv'),
     (AdultDataset, 'adult.csv')
     ]
 
 ALGORITHMS: SamplingAlgorithm = [
-    SmoteAlgorithm
+    McdAdasynAlgorithm,
+    McdSmoteAlgorithm
     ]
 
 MODELS = [
