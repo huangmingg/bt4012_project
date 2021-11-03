015f80dcc868837d289fca73721156c8290a9186
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-03 11:56:51 +0800

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -2,6 +2,8 @@ from preprocess.preprocess import CreditCardDataset, AdultDataset
 from sampling.baseline import BaselineAlgorithm
 from sampling.robrose import RobRoseAlgorithm
 from sampling.smote import SmoteAlgorithm
+from sampling.mcd_adasyn import McdAdasyn
+from sampling.mcd_smote import McdSmote
 from sampling.sampling import SamplingAlgorithm
 from model.xgb_model import XGBWrapper
 from model.lg_model import LGWrapper
