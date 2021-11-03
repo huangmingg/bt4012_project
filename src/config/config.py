fa189bea3b3727408e064dc2574664377bc0ee4a
huangmingg <68838289+huangmingg@users.noreply.github.com>
2021-11-03 12:40:31 +0800

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,15 +1,16 @@
-from preprocess.preprocess import CreditCardDataset, AdultDataset
+from model.model import ClassifierWrapper
+from model.xgb_model import XGBWrapper
+from model.lg_model import LGWrapper
+from preprocess.preprocess import CreditCardDataset, AdultDataset, DatasetWrapper
 from sampling.baseline import BaselineAlgorithm
 from sampling.robrose import RobRoseAlgorithm
 from sampling.smote import SmoteAlgorithm
 from sampling.mcd_adasyn import McdAdasynAlgorithm
 from sampling.mcd_smote import McdSmoteAlgorithm
 from sampling.sampling import SamplingAlgorithm
-from model.xgb_model import XGBWrapper
-from model.lg_model import LGWrapper
 
 
-DATASETS = [
+DATASETS: DatasetWrapper = [
     (CreditCardDataset, 'creditcard.csv'),
     (AdultDataset, 'adult.csv')
     ]
@@ -19,7 +20,7 @@ ALGORITHMS: SamplingAlgorithm = [
     McdSmoteAlgorithm
     ]
 
-MODELS = [
+MODELS: ClassifierWrapper = [
     XGBWrapper,
     LGWrapper
     ]
