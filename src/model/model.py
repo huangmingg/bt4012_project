bed7ff7a4f0ef7f600aa30a607f5c88cb2f4c27f
huangmingg <huangmingg@hotmail.com>
2021-11-04 13:52:13 +0800

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from preprocess.preprocess import DatasetWrapper
-from typing import Tuple
+from typing import Tuple, List
 import pickle
 import numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score
@@ -10,20 +10,26 @@ class ClassifierWrapper(ABC):
 
     def __init__(self, data: DatasetWrapper) -> None:
         self.data = data
-        self.result = []
+        self.result = {}
 
     @abstractmethod
     def evaluate(self) -> None:
-        for i in range(len(self.data.bxt)):
-            self.model.fit(self.data.bxt[i], self.data.byt[i])
-            y_score = self.model.predict_proba(self.data.x_test[i])
-            y_pred = y_score[:,1]
-            rocauc = roc_auc_score(self.data.y_test[i], y_pred)
-            auprc = average_precision_score(self.data.y_test[i], y_pred)
-            self.result.append({'rocauc': rocauc, 'auprc': auprc})
+        for l in self.data.b.keys():
+            self.result[l] = []
+            for idx, (x, y) in enumerate(self.data.b[l]):
+                self.model.fit(x, y)
+                y_score = self.model.predict_proba(self.data.x_test[idx])
+                y_pred = y_score[:,1]
+                rocauc = roc_auc_score(self.data.y_test[idx], y_pred)
+                auprc = average_precision_score(self.data.y_test[idx], y_pred)
+                self.result[l].append((rocauc, auprc))
 
 
-    def compute_results(self) -> Tuple[np.float, np.float, np.float, np.float]:
-        rocauc = np.array(list(map(lambda x: x.get('rocauc'), self.result)))
-        auprc = np.array(list(map(lambda x: x.get('auprc'), self.result)))
-        return round(np.mean(rocauc), 3), round(np.std(rocauc), 3), round(np.mean(auprc), 3), round(np.std(auprc), 3)
+    def compute_results(self) -> List[Tuple[str, Tuple[np.float, np.float, np.float, np.float]]]:
+        output = []
+        for j in self.result.keys():
+            rocauc = np.array(list(map(lambda x: x[0], self.result[j])))
+            auprc = np.array(list(map(lambda x: x[1], self.result[j])))
+            output.append((str(j), (round(np.mean(rocauc), 3), round(np.std(rocauc), 3), 
+            round(np.mean(auprc), 3), round(np.std(auprc), 3))))
+        return output
