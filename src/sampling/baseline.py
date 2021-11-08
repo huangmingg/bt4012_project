02cf8c82c3f713964cbbd3206b8c813e554e9501
Rance <limjyrance@gmail.com>
2021-11-08 17:32:05 +0800

    Revert baseline to leave as imbalanced

--- src/sampling/baseline.py
+++ src/sampling/baseline.py
@@ -1,5 +1,4 @@
 import numpy as np
-from imblearn.over_sampling import RandomOverSampler
 from sampling.sampling import SamplingAlgorithm
 from typing import Tuple, List
 
@@ -9,6 +8,4 @@ class BaselineAlgorithm(SamplingAlgorithm):
     @staticmethod
     def run(x_train: np.array, y_train: np.array, columns: List[str], oversampling_level: float = 0.5, 
     random_state: int = 4012, **kwargs) -> Tuple[np.array, np.array]:
-        oversample = RandomOverSampler(sampling_strategy=oversampling_level, random_state=random_state)
-        bxt, byt = oversample.fit_resample(x_train, y_train)
-        return bxt, byt
+        return x_train, y_train
