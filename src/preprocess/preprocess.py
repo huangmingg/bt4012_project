898a10bfb3db3c6f203e0f7eafa1fb4e09e687d5
Rance <limjyrance@gmail.com>
2021-11-08 17:10:36 +0800

    Add kwargs to preprocess

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 from sampling.sampling import SamplingAlgorithm
 from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
 from sklearn.preprocessing import StandardScaler
-from typing import Tuple
+from typing import Tuple, List
 
 
 class DatasetWrapper(ABC):
@@ -60,7 +60,7 @@ class SwarmDataset(DatasetWrapper):
     def __init__(self, filepath: os.path) -> None:
         super().__init__(filepath)
 
-    def preprocess(self) -> None:
+    def preprocess(self, **kwargs) -> None:
         super().preprocess()
         self.raw_df = self.raw_df[~self.raw_df.duplicated(keep='last')].head(2300)
         y = self.raw_df['Swarm_Behaviour']
@@ -82,15 +82,14 @@ class AdultDataset(DatasetWrapper):
         def __init__(self, filepath: os.path) -> None:
             super().__init__(filepath)
 
-        def preprocess(self) -> None:
+        def preprocess(self, **kwargs) -> None:
             super().preprocess()
             self.raw_df[self.raw_df=='?'] = np.nan
             self.raw_df = self.raw_df.dropna(subset=['workclass', 'occupation', 'native.country'])
             self.raw_df = self.raw_df.drop_duplicates()
-
+            self.raw_df.income = self.raw_df.income.map({'<=50K': 0, '>50K':1})
             # identify numerical and categorical columns
             x = self.raw_df.drop('income', axis=1)
-            y = self.raw_df.income
             self.columns = x.columns
             self.num_columns = ['age', 'fnlwgt', 'education.num', 'capital.gain', 'capital.loss', 'hours.per.week']
             self.cat_columns = ['workclass', 'education', 'marital.status', 'occupation', 'relationship' ,'race', 'sex', 'native.country']  
@@ -98,6 +97,7 @@ class AdultDataset(DatasetWrapper):
             self.cat_columns_ind = [self.columns.tolist().index(cat) for cat in self.cat_columns]      
 
             x = x.to_numpy()
+            y = self.raw_df.income.to_numpy()
             scaler = StandardScaler()
             self.x_train, self.x_test, self.y_train, self.y_test = [], [], [], []
             r = RepeatedStratifiedKFold(n_splits=self.n_splits, n_repeats=self.n_repeats, random_state=self.random_state)
