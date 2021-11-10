44166e38e94cf7ecc346d3852fd6fe44771982b8
huangmingg <huangmingg@hotmail.com>
2021-11-11 01:14:27 +0800

    update

--- src/experiment_1.py
+++ src/experiment_1.py
@@ -1,8 +1,7 @@
 import os
 import pandas as pd
-import timeit
+from model.decision_tree_model import DecisionTreeWrapper
 from model.lg_model import LGWrapper
-from model.xgb_model import XGBWrapper
 from model.model import ClassifierWrapper
 from preprocess.preprocess import CreditCardDataset
 from sampling.adasyn import AdasynAlgorithm
@@ -10,39 +9,59 @@ from sampling.baseline import BaselineAlgorithm
 from sampling.mcd_adasyn import McdAdasynAlgorithm
 from sampling.mcd_smote import McdSmoteAlgorithm
 from sampling.robrose import RobRoseAlgorithm
-from sampling.smote_boost import SMOTEBoostWrapperAlgorithm
 from sampling.sampling import SamplingAlgorithm
 from sampling.smote import SmoteAlgorithm
 
 
 def main():
+    OVERSAMPLING_LEVEL = [0.01, 0.05, 0.1, 0.5, 1]
+    RANDOM_STATE = 4012
+
+    MODELS: ClassifierWrapper = [
+        LGWrapper,
+        DecisionTreeWrapper,
+    ]
+
     DATASETS = [
-        (CreditCardDataset, "creditcard.csv", {"random_state": 4012, "n_repeats": 2, "n_splits": 5}),
+        (CreditCardDataset, "creditcard.csv", {"random_state": RANDOM_STATE, "n_repeats": 2, "n_splits": 5}),
     ]
 
     ALGORITHMS: SamplingAlgorithm = [
-        # (BaselineAlgorithm, {"random_state": 4012, "oversampling_level": [1]}),
-        (SMOTEBoostWrapperAlgorithm, {"random_state": 4012, "oversampling_level": [1]}),
-        # (SmoteAlgorithm, {"random_state": 4012, "oversampling_level": [1]}),
-        # (AdasynAlgorithm, {"random_state": 4012, "oversampling_level": [1]}),
-        # (RobRoseAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "alpha": 0.95, "const": 1}),
-        # (McdAdasynAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "sp": 0.95}),
-        # (McdSmoteAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "sp": 0.95, "p": 0.999}),
+        (BaselineAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (SmoteAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (AdasynAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (RobRoseAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "alpha": 0.95, "const": 1}),
+        (McdAdasynAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "sp": 0.95}),
+        (McdSmoteAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "sp": 0.95, "p": 0.999}),
     ]
 
-    NUM_RUNS = 1
-    result = []
+    results = []
 
-    for d, fp, p in DATASETS:
-        d = d(fp)
-        d.preprocess(**p)
-        # for a in ALGORITHMS:
-        #     duration = timeit.Timer(lambda: d.balance(a[0], **a[1])).timeit(number=NUM_RUNS)
-        #     avg_duration = duration / NUM_RUNS
-        #     result.append({'algo': a[0].__name__, 'duration': avg_duration})
-        #     print(f'On average it took {avg_duration} seconds')
-                
-            
+    for m in MODELS:
+        for d, fp, p in DATASETS:
+            d = d(fp)
+            d.preprocess(**p)
+            for a in ALGORITHMS:
+                d.balance(a[0], **a[1])
+                model = m(d)
+                print(f"Evaluating {type(model).__name__} model for algorithm {a[0].__name__} using dataset {type(d).__name__}")
+                model.evaluate()
+                res = model.compute_results()
+                for l, val in res:
+                    results.append(
+                        {
+                            "algo": a[0].__name__,
+                            "model": type(model).__name__,
+                            "dataset": type(d).__name__,
+                            "sampling_ratio": {l},
+                            "rocauc_mean": val[0],
+                            "rocauc_std": val[1],
+                            "auprc_mean": val[2],
+                            "auprc_std": val[3]     
+                        }
+                    )
+                    print(f"At oversampling ratio {l}, ROCAUC Mean: {val[0]}, ROCAUC Std: {val[1]}, AUPRC Mean: {val[2]}, AUPRC Std: {val[3]}")
+
 
 if __name__ == '__main__':
     main()
--- src/experiment_3.py
+++ src/experiment_3.py
@@ -1,111 +1,84 @@
-import copy
+import os
 
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import roc_auc_score, average_precision_score
+from preprocess.preprocess import CreditCardDataset, SwarmDataset
 
-
-from preprocess.preprocess import AdultDataset
+from model.model import ClassifierWrapper
+from preprocess.preprocess import DatasetWrapper
 from sampling.sampling import SamplingAlgorithm
 from sampling.baseline import BaselineAlgorithm
-from sampling.robrose import RobRoseAlgorithm
-from sampling.smote import SmotencAlgorithm
-from sampling.adasyn import AdasynNCAlgorithm
-from model.model import ClassifierWrapper
+from sampling.smote import SmoteAlgorithm
+from sampling.adasyn import AdasynAlgorithm
 from model.lg_model import LGWrapper
 from model.decision_tree_model import DecisionTreeWrapper
+from sklearn.metrics import roc_auc_score, average_precision_score
 
-
-def main():
-    """
-    Evaluates SMOTENC, ADASYNNC and robROSE on Adult Dataset (at various imbalance levels). Different oversampling levels are used to assess the algorithm's effectiveness.
-    """
+def dataInfo():
+    dataset = SwarmDataset("Swarm_Behaviour.csv")
+    print("High Dimensional Dataset")
+    print("Number of Positives: ", len(dataset.raw_df[dataset.raw_df['Swarm_Behaviour']==1]))
+    print("Number of Negatives: ", len(dataset.raw_df[dataset.raw_df['Swarm_Behaviour']==0]))
+    print("Proportion of Postives over the whole dataset: ", len(dataset.raw_df[dataset.raw_df['Swarm_Behaviour']==1])/len(dataset.raw_df))
+    print("Proportion of Postives over the majority: ", len(dataset.raw_df[dataset.raw_df['Swarm_Behaviour']==1])/len(dataset.raw_df[dataset.raw_df['Swarm_Behaviour']==0]))
+    dataset = CreditCardDataset("creditcard.csv")
+    print("Low Dimensional Dataset")
+    print("Number of Positives: ", len(dataset.raw_df[dataset.raw_df['Class']==1]))
+    print("Number of Negatives: ", len(dataset.raw_df[dataset.raw_df['Class']==0]))
+    print("Proportion of Postives over the whole dataset: ", len(dataset.raw_df[dataset.raw_df['Class']==1])/len(dataset.raw_df))
+    print("Proportion of Postives over the majority: ", len(dataset.raw_df[dataset.raw_df['Class']==1])/len(dataset.raw_df[dataset.raw_df['Class']==0]))
+        
+def experiment_3():
+    
     MODELS: ClassifierWrapper = [
         LGWrapper,
         DecisionTreeWrapper,
     ]
 
-    DATASETS_1 = [(AdultDataset, "adult.csv", {"random_state": 4012, "n_repeats": 2, "n_splits": 5})] # Original
-    DATASETS_2 = [(AdultDataset, "adult.csv", {"random_state": 4012, "imbal_level": 0.05, "n_repeats": 2, "n_splits": 5})] # Imbalance level 5%
-    DATASETS_3 = [(AdultDataset, "adult.csv", {"random_state": 4012, "imbal_level": 0.01, "n_repeats": 2, "n_splits": 5})] # Imbalance level 1%
-
-
-    # For original imbalance level
-    ALGORITHMS_1: SamplingAlgorithm = [
-        (BaselineAlgorithm, {}),
-        (SmotencAlgorithm, {"random_state": 4012,
-                            "oversampling_level": [1.0]}),
-        (AdasynNCAlgorithm, {"random_state": 4012,
-                             "oversampling_level": [1.0] }),
-        (RobRoseAlgorithm, {"random_state": 4012, "oversampling_level": [1.0], "alpha": 0.95, "const": 1}),
+    DATASETS = [
+        (SwarmDataset, "Swarm_Behaviour.csv", {"random_state": 4012,
+                                     "n_repeats": 1,
+                                     "imbal_level": None,
+                                     "n_splits": 5}
+         ),
+        (SwarmDataset, "Swarm_Behaviour.csv", {"random_state": 4012,
+                                     "imbal_level": 0.1,
+                                     "n_repeats": 1,
+                                     "n_splits": 5}
+         ),
+        (SwarmDataset, "Swarm_Behaviour.csv", {"random_state": 4012,
+                                     "imbal_level": 0.05,
+                                     "n_repeats": 1,
+                                     "n_splits": 5}
+         ),
+        (SwarmDataset, "Swarm_Behaviour.csv", {"random_state": 4012,
+                                     "imbal_level": 0.01,
+                                     "n_repeats": 1,
+                                     "n_splits": 5}
+         ),
     ]
 
-    # For imbalance level of 5%
-    ALGORITHMS_2: SamplingAlgorithm = [
+    ALGORITHMS: SamplingAlgorithm = [
         (BaselineAlgorithm, {}),
-        (SmotencAlgorithm, {"random_state": 4012,
-                            "oversampling_level": [1/9, 3/7, 1.0]}),
-        (AdasynNCAlgorithm, {"random_state": 4012,
-                             "oversampling_level": [1/9, 3/7, 1.0] }),
-        (RobRoseAlgorithm, {"random_state": 4012, "oversampling_level": [1/9, 3/7, 1.0], "alpha": 0.95, "const": 1}),
+        (SmoteAlgorithm, {"random_state": 4012, "oversampling_level": [0.7, 0.8, 0.9]}),
+        (AdasynAlgorithm, {"random_state": 4012, "oversampling_level": [0.7, 0.8, 0.9]}),
     ]
 
-    # For imbalance level of 1%
-    ALGORITHMS_3: SamplingAlgorithm = [
-        (BaselineAlgorithm, {}),
-        (SmotencAlgorithm, {"random_state": 4012,
-                            "oversampling_level": [1/19, 1/9, 3/7, 1.0]}),
-        (AdasynNCAlgorithm, {"random_state": 4012,
-                             "oversampling_level": [1/19, 1/9, 3/7, 1.0] }),
-        (RobRoseAlgorithm, {"random_state": 4012, "oversampling_level": [1/19, 1/9, 3/7, 1.0], "alpha": 0.95, "const": 1}),
-    ]   
-
-    EXPERIMENTS = zip(['Original', '5%', '1%'], [DATASETS_1, DATASETS_2, DATASETS_3], [ALGORITHMS_1, ALGORITHMS_2, ALGORITHMS_3])
-
-    for label, datasets, algorithms in EXPERIMENTS:
-        print(f'Experimenting with {label} imbalance level:')
-        experiment_2(MODELS, datasets, algorithms)
-
-
-
-def experiment_2(MODELS, DATASETS, ALGORITHMS):
     for m in MODELS:
         for d, fp, p in DATASETS:
             d = d(fp)
             d.preprocess(**p)
-
-            fold_ohes = []       
-            for idx, x_fold in enumerate(d.x_train):
-                ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
-                # Fit on train fold (do not transform since oversampling is done with categorical directly)
-                # Safe to fit on train fold before oversampling since oversampling will not introduce new categories
-                ohe.fit(x_fold[:, d.cat_columns_ind]) 
-                x_test = d.x_test[idx]
-                x_test_ohe = ohe.transform(x_test[:, d.cat_columns_ind]) # Transform test fold
-                new_x_test = np.hstack([x_test[:, d.num_columns_ind], x_test_ohe])
-                d.x_test[idx] = new_x_test
-                fold_ohes.append(ohe)  # Store OHE used for each fold to encode balanced dataset after oversampling
-
-
+            if p["imbal_level"]:
+                print("Imbalanced Level:" + str(p["imbal_level"]))
             for a in ALGORITHMS:
-                d.balance(a[0], categorical_features=d.cat_columns_ind, **a[1])
-                
-                # One Hot Encode the balanced datasets prior to evaluation (only for Experiment 2)
-                for level in d.b.keys(): # For each oversampling level
-                    for idx, (x, y) in enumerate(d.b[level]):  # For each fold
-                        
-                        # OHE balanced dataset using the OHE fit on that fold's train data
-                        bxt_ohe = fold_ohes[idx].transform(x[:, d.cat_columns_ind])
-                        new_bxt = np.hstack([x[:, d.num_columns_ind], bxt_ohe])
-                        d.b[level][idx] = new_bxt, y
-
+                d.balance(a[0], **a[1])
                 model = m(d)
                 print(f"Evaluating {type(model).__name__} model for algorithm {a[0].__name__} using dataset {type(d).__name__}")
                 model.evaluate()
                 res = model.compute_results()
                 for l, val in res:
                     print(f"At oversampling ratio {l}, ROCAUC Mean: {val[0]}, ROCAUC Std: {val[1]}, AUPRC Mean: {val[2]}, AUPRC Std: {val[3]}")
-
+            
+        
 if __name__ == '__main__':
-    main()
-
+    experiment_3()        
+        
