0e1c2d6c58111a97048f629c4d474dc078d2a039
huangmingg <huangmingg@hotmail.com>
2021-11-11 00:36:22 +0800

    Update experiment_1.py

--- src/experiment_1.py
+++ src/experiment_1.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
+from model.decision_tree_model import DecisionTreeWrapper
 from model.lg_model import LGWrapper
-from model.xgb_model import XGBWrapper
 from model.model import ClassifierWrapper
 from preprocess.preprocess import CreditCardDataset
 from sampling.adasyn import AdasynAlgorithm
@@ -14,23 +14,28 @@ from sampling.smote import SmoteAlgorithm
 
 
 def main():
+    OVERSAMPLING_LEVEL = [0.01, 0.05, 0.1, 0.5, 1]
+    RANDOM_STATE = 4012
+
     MODELS: ClassifierWrapper = [
         LGWrapper,
-        XGBWrapper,
+        DecisionTreeWrapper,
     ]
 
     DATASETS = [
-        (CreditCardDataset, "creditcard.csv", {"random_state": 4012, "n_repeats": 2, "n_splits": 5}),
+        (CreditCardDataset, "creditcard.csv", {"random_state": RANDOM_STATE, "n_repeats": 2, "n_splits": 5}),
     ]
 
     ALGORITHMS: SamplingAlgorithm = [
-        (BaselineAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5]}),
-        (SmoteAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5]}),
-        (AdasynAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5]}),
-        (RobRoseAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "alpha": 0.95, "const": 1}),
-        (McdAdasynAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "sp": 0.95}),
-        (McdSmoteAlgorithm, {"random_state": 4012, "oversampling_level": [0.05, 0.1, 0.2, 0.5], "sp": 0.95, "p": 0.999}),
+        (BaselineAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (SmoteAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (AdasynAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL}),
+        (RobRoseAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "alpha": 0.95, "const": 1}),
+        (McdAdasynAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "sp": 0.95}),
+        (McdSmoteAlgorithm, {"random_state": RANDOM_STATE, "oversampling_level": OVERSAMPLING_LEVEL, "sp": 0.95, "p": 0.999}),
     ]
+
+    results = []
 
     for m in MODELS:
         for d, fp, p in DATASETS:
@@ -43,8 +48,20 @@ def main():
                 model.evaluate()
                 res = model.compute_results()
                 for l, val in res:
+                    results.append(
+                        {
+                            "algo": a[0].__name__,
+                            "model": type(model).__name__,
+                            "dataset": type(d).__name__,
+                            "sampling_ratio": {l},
+                            "rocauc_mean": val[0],
+                            "rocauc_std": val[1],
+                            "auprc_mean": val[2],
+                            "auprc_std": val[3]     
+                        }
+                    )
                     print(f"At oversampling ratio {l}, ROCAUC Mean: {val[0]}, ROCAUC Std: {val[1]}, AUPRC Mean: {val[2]}, AUPRC Std: {val[3]}")
-            
+
 
 if __name__ == '__main__':
     main()
